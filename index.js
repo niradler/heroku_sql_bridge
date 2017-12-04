a93e963692e25524c2e2a2a3be75e4bd41e8f90d
MOONSITE-AD\nira <nira@moonsite.com>
2017-12-04 13:29:05 +0200

    get PORT

--- index.js
+++ index.js
@@ -81,8 +81,9 @@ app.post('/bridge',jsonParser, function (request, response) {
         });
     }
  })
-var server = app.listen(8081, function () {
-   var host = server.address().address
+
+var server = app.listen(process.env.PORT || 8080, function () {
+   var host = server.address().address || 'localhost';
    var port = server.address().port
    
    console.log("Example app listening at http://%s:%s", host, port)
